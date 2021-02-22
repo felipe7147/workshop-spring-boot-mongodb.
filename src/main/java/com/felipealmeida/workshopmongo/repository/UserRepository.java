62f9e41fe81eb3c0380d2f39f2b5f381ceaa29ab
felipe almeida <felipe almeida>
2021-02-22 10:43:15 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/felipealmeida/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.felipealmeida.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.felipealmeida.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
