6a762718e4ddf1443db458b71039ebd68ba93205
felipe almeida <felipe almeida>
2021-02-22 21:48:29 -0300

    obtendo um post por id

---
+++ src/main/java/com/felipealmeida/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.felipealmeida.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipealmeida.workshopmongo.domain.Post;
+import com.felipealmeida.workshopmongo.repository.PostRepository;
+import com.felipealmeida.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
+	
+	
+}
